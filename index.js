9b249fb5ba0b8da13cb892b1adbb3cb231e94467
mie6996 <nguyenxuanphat060301@gmail.com>
2022-06-10 15:29:03 +0700

    deploy

---
+++ index.js
@@ -0,0 +1,4 @@
+'use strict';
+import app from './src/app.js'
+
+app.listen(3000, () => console.log('Local app listening on port 3000!'));
--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { engine } from 'express-handlebars'
 import { dirname } from 'path'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import serverless from 'serverless-http'
 import { connectDB } from "./lib/mongodb.js"
 
 const __filename = fileURLToPath(import.meta.url)
@@ -11,50 +12,52 @@ const __dirname = dirname(__filename)
 const app = express()
 const port = process.env.PORT || 3000
 
-connectDB()
+// connectDB()
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('hbs', engine({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 
+const router = express.Router()
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/shop', (req, res) => {
+router.get('/shop', (req, res) => {
     res.render('shop')
 })
 
-app.get('/about', (req, res) => {
+router.get('/about', (req, res) => {
     res.render('about')
 })
 
-app.get('/shop-details', (req, res) => {
+router.get('/shop-details', (req, res) => {
     res.render('shop-details')
 })
 
-app.get('/shopping-cart', (req, res) => {
+router.get('/shopping-cart', (req, res) => {
     res.render('shopping-cart')
 })
 
-app.get('/checkout', (req, res) => {
+router.get('/checkout', (req, res) => {
     res.render('checkout')
 })
 
-app.get('/blog', (req, res) => {
+router.get('/blog', (req, res) => {
     res.render('blog')
 })
 
-app.get('/blog-details', (req, res) => {
+router.get('/blog-details', (req, res) => {
     res.render('blog-details')
 })
 
-app.get('/contact', (req, res) => {
+router.get('/contact', (req, res) => {
     res.render('contact')
 })
 
-app.listen(port, () => {
-    console.log(`[Server] is running ${port}`)
-})
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+export default app
+export const handler = () => serverless()
