4d6ac7006d7e727fecf31f5d1dcd9af348586496
mie6996 <nguyenxuanphat060301@gmail.com>
2022-06-22 22:49:44 +0700

    add router

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 // get directory path of the current file
 const __dirname = path.dirname(__filename)
+import routes from './routes/index.js'
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -19,45 +20,7 @@ app.engine('hbs', engine({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 
-const router = express.Router()
-
-router.get('/', (req, res) => {
-    res.render('index')
-})
-
-router.get('/shop', (req, res) => {
-    res.render('shop')
-})
-
-router.get('/about', (req, res) => {
-    res.render('about')
-})
-
-router.get('/shop-details', (req, res) => {
-    res.render('shop-details')
-})
-
-router.get('/shopping-cart', (req, res) => {
-    res.render('shopping-cart')
-})
-
-router.get('/checkout', (req, res) => {
-    res.render('checkout')
-})
-
-router.get('/blog', (req, res) => {
-    res.render('blog')
-})
-
-router.get('/blog-details', (req, res) => {
-    res.render('blog-details')
-})
-
-router.get('/contact', (req, res) => {
-    res.render('contact')
-})
-
-app.use(router);
+app.use(routes);
 
 app.listen(port, () => {
     console.log("[Server] is running on port " + port);
---
+++ src/routes/index.js
@@ -0,0 +1,58 @@
+import express from 'express'
+import aboutRoute from './about.route.js'
+import blogRoute from './blog.route.js'
+import blogDetailsRoute from './blogDetails.route.js'
+import checkOutRoute from './checkOut.route.js'
+import contactRoute from './contact.route.js'
+import shopRoute from './shop.route.js'
+import shopDetailsRoute from './shopDetails.route.js'
+import shoppingCartRoute from './shoppingCart.route.js'
+
+const router = express.Router()
+
+const defaultRoutes = [
+    {
+        path: '/',
+        route: (req, res) => {
+            res.render('index');
+        },
+    },
+    {
+        path: '/shop',
+        route: shopRoute,
+    },
+    {
+        path: '/about',
+        route: aboutRoute,
+    },
+    {
+        path: '/shop-details',
+        route: shopDetailsRoute,
+    },
+    {
+        path: '/shopping-cart',
+        route: shoppingCartRoute,
+    },
+    {
+        path: '/checkout',
+        route: checkOutRoute,
+    },
+    {
+        path: '/blog',
+        route: blogRoute,
+    },
+    {
+        path: 'blog-details',
+        route: blogDetailsRoute,
+    },
+    {
+        path: '/contact',
+        route: contactRoute,
+    },
+];
+
+defaultRoutes.forEach((route) => {
+    router.use(route.path, route.route)
+})
+
+export default router
